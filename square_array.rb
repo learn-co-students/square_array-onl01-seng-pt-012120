2b6eed8f9298425c06bb90d79223b5a230a31a6f
Chris Beyer <beyercorpuz@gmail.com>
2020-01-29 02:06:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-  new_array = [1,4,5learn ]
+  new_array = [1,4,5]
   counter = 3
   while array[counter] do #returns nil to evaluate to false
     new_array << array[counter] ** 2
